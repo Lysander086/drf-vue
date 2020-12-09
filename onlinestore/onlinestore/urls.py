ceed2c733e7fd533a5e46f0948aa84f8d8e1ba2b
z <z@example.com>
2020-12-09 20:49:27 +0800

    c2 2. Build Your First REST API with Django   success list view on localhost:8000/

--- onlinestore/onlinestore/urls.py
+++ onlinestore/onlinestore/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("products.urls"))
+    path("", include("products.urls"))
 ]
 
 if settings.DEBUG:
