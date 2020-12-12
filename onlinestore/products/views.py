a8135cbc8c60148614a2f4e25db40e9d93503c4a
z <19286863+Lysander686@users.noreply.github.com>
2020-12-12 14:51:12 +0800

    product list complete

--- onlinestore/products/views.py
+++ onlinestore/products/views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-from .models import Product, Manufacturer
+
+from .models import Product
 
 
 def product_list(request):
@@ -11,11 +10,27 @@ def product_list(request):
     return response
 
 
-# class ProductDetailView(DetailView):
-#     model = Product
-#     template_name = 'products/product_detail.html'
-#
-#
-# class ProductListView(ListView):
-#     model = Product
-#     template_name = 'products/product_list.html'
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {"product": {
+            "name": product.name,
+            "manufacturer": product.manufacturer,
+            "description": product.description,
+            "photo": product.photo.url,
+            "price": product.price,
+            "shipping_cost": product.shipping_cost,
+            "quantity": product.quantity,
+        }}
+
+        response = JsonResponse(data)
+
+    except Product.DoesNotExist:
+        response = JsonResponse({
+            "error": {
+                "code": 404,
+                "message": "product not found"
+            }
+        }, status=404)
+
+    return response
