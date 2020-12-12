3fd0c1c47b7dfcf60480569f68ca97039ab5771f
z <19286863+Lysander686@users.noreply.github.com>
2020-12-12 16:08:46 +0800

    manufactureres complete

--- onlinestore/products/urls.py
+++ onlinestore/products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import product_detail
+from .views import product_detail, manufacturer_detail,manufacturer_list
 from .views import product_list
 
 urlpatterns = [
     path("products/", product_list, name="product-list"),
     path("products/<int:pk>/", product_detail, name="product-detail"),
-
+    path("manufacturers/", manufacturer_list, name="manufacturer_list"),
+    path("manufacturers/<int:pk>/", manufacturer_detail, name="manufacturer_detail"),
 
 ]
--- onlinestore/products/views.py
+++ onlinestore/products/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 
-from .models import Product
+from .models import Product, Manufacturer
 
 
 def product_list(request):
@@ -34,3 +34,32 @@ def product_detail(request, pk):
         }, status=404)
 
     return response
+
+
+def manufacturer_list(req):
+    manufacturers = Manufacturer.objects.all()
+    data = {
+        "manufacturers": list(manufacturers.values("pk", "name"))
+    }
+    res = JsonResponse(data)
+    return res
+
+
+def manufacturer_detail(req, pk):
+    try:
+        manufacturer = Manufacturer.objects.get(pk=pk)
+        data = {"manufacturer": {
+            "name": manufacturer.name,
+            "location": manufacturer.location,
+            "active": manufacturer.active,
+        }}
+
+        return JsonResponse(data)
+
+    except Manufacturer.DoesNotExist:
+        return JsonResponse({
+            "error": {
+                "code": 404,
+                "message": "not found"
+            }
+        }, status=404)
