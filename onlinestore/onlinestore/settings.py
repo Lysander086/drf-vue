2520ca6f01d5256b9b085ba5de78691ef4fbb72c
Lysander <19286863+Lysander686@users.noreply.github.com>
2020-12-19 23:10:52 +0800

    integrate auth

--- onlinestore/onlinestore/settings.py
+++ onlinestore/onlinestore/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'products',
     'news',
     'ebooks',
@@ -119,8 +120,9 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Shanghai'
 
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 3
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
