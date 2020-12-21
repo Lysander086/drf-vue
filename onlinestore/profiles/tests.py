776816cbe4ec99854e18bee429b74973dc432547
Lysander <19286863+Lysander686@users.noreply.github.com>
2020-12-22 01:55:31 +0800

    s9 autmate test - 1

--- onlinestore/profiles/tests.py
+++ onlinestore/profiles/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+import json
+from django.contrib.auth.models import User
+
+from django.urls import reverse
+
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from rest_framework import status
+
+from .models import Profile
+from .serializers import ProfileSerializer
+
+
+class RegistrationTestCase(APITestCase):
+    def test_registration(self):
+        data = {"username": "testcase", "email": "test@localhost.app", "password1": "some_Strong_pw",
+                "password2": "some_Strong_pw"}
+        response = self.client.post("/api/rest-auth/registration/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
