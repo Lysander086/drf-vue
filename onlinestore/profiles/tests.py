09ffb4dcbbf4fa241c10babc882c9bd29b011f2e
Lysander <19286863+Lysander686@users.noreply.github.com>
2020-12-22 02:24:36 +0800

    s9 test sucess

--- onlinestore/profiles/tests.py
+++ onlinestore/profiles/tests.py
@@ -22,3 +22,28 @@ class RegistrationTestCase(APITestCase):
         response = self.client.post("/api/rest-auth/registration/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+
+class ProfileViewSetTestCase(APITestCase):
+    list_url = reverse("profile-list")
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="Davinci", password="some_Strong_pw")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+    def test_profile_list_authenticated(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_profile_list_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    # def test_profile_detail_retrieve(self):
+    #     response = self.client.get(reverse("profile-detail", kwargs={"pk":1}))
+    #     rezp
+
