df62c9327afb29282cd58cc5447c65778ae0b19a
Lysander <19286863+Lysander686@users.noreply.github.com>
2020-12-23 16:43:42 +0800

    create brand new project

---
+++ previous/clients/clients/config.py
@@ -0,0 +1,5 @@
+#  self config here, wont be commit to git
+admin_password = "Password1"
+password_test = "config.password_testA"
+my_token  = "Token 0dbd700a05f09a3b8cb3748347b4c733d414b809"
+my_token3  = "Token 8d14c8b43b0e5f31db422122708b9b2fbf6d7f7a"
